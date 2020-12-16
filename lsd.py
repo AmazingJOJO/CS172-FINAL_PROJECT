1abefb880e9fded2ef73b676443c294db071bae6
FuHongju <fuhj@shanghaitech.edu.cn>
2020-12-16 21:09:46 +0800

    update lsd.py

--- lsd.py
+++ lsd.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 import os
 from pylsd.lsd import lsd
-fullName = '000002.jpg'
+fullName = '000033.jpg'
 folder, imgName = os.path.split(fullName)
 src = cv2.imread(fullName, cv2.IMREAD_COLOR)
+height = src.shape[1]
+weight = src.shape[0]    
+image = cv2.imread(fullName, cv2.IMREAD_COLOR)
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 lines = lsd(gray)
 for i in xrange(lines.shape[0]):
@@ -19,17 +22,27 @@ ret, binary = cv2.threshold(gray,10,255,cv2.THRESH_BINARY)
  
 contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 #print("coun:",contours[0],contours[1])
-c = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+
+c1 = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+c2 = sorted(contours, key=cv2.contourArea, reverse=True)[1]
 #print("c:",c[0],c[1],c[2],c[3])
 # compute the rotated bounding box of the largest contour
-x, y, w, h = cv2.boundingRect(c)
-"""for c in contours:
+
+x1, y1, w1, h1 = cv2.boundingRect(c1)
+x2, y2, w2, h2 = cv2.boundingRect(c2)
+if (w1 * h1) / (height * weight) > 0.95:
+    print(w1, h1)
+    print(height,weight)
+    crop_img = image[y2:y2 + h2, x2:x2 + w2]
+else:
+    crop_img = image[y1:y1 + h1, x1:x1 + w1]
+for c in contours:
     # find bounding box coordinates
     
     x, y, w, h = cv2.boundingRect(c)
     
-    cv2.rectangle(src, (x,y), (x+w, y+h), (0, 255, 0), 2) """
-cv2.rectangle(src, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(src, (x,y), (x+w, y+h), (0, 255, 0), 2) 
+#cv2.rectangle(src, (x,y), (x+w, y+h), (0, 255, 0), 2)
 #draw_img = cv2.drawContours(src.copy(), [box], -1, (0, 0, 255), 2)
 """Xs = [i[0] for i in box]
 Ys = [i[1] for i in box]
@@ -44,8 +57,6 @@ crop_img = src[y1:y1+hight, x1:x1+width]"""
 #print(src.shape)
 
 
-
-crop_img = src[y:y + h, x:x + w]
 cv2.imshow("or",src)
 cv2.imshow("result", crop_img)
 #cv2.imshow("contour", draw_img)  
