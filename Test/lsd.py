9e61e80a79e11f672c40cf4ab74eb9afb5d5ac7d
FuHongju <fuhj@shanghaitech.edu.cn>
2020-12-26 23:27:10 +0800

    update lsd.py

--- Test/lsd.py
+++ Test/lsd.py
@@ -3,6 +3,14 @@ import numpy as np
 import os
 from pylsd.lsd import lsd
 
+class contourImage():
+    def __init__(self, flagList,tag):
+        self.flagList = flagList
+        self.tag = []
+        for i in range(len(self.flagList)):
+            self.tag.append(tag)
+            
+
 def Gaussian_Blur(gray):
     blurred = cv2.GaussianBlur(gray, (7, 7),0)
     
@@ -17,7 +25,7 @@ def FindApple(img):
     weight = src.shape[0]    
     image = cv2.imread(img, cv2.IMREAD_COLOR)
     src = Gaussian_Blur(image)
-    cv2.imshow("blur",src)
+    #cv2.imshow("blur",src)
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     blur = Gaussian_Blur(gray)
     lines = lsd(blur)
@@ -25,16 +33,17 @@ def FindApple(img):
         pt1 = (int(lines[i, 0]), int(lines[i, 1]))
         pt2 = (int(lines[i, 2]), int(lines[i, 3]))
         width = lines[i, 4]
-        cv2.line(blur, pt1, pt2, (0,0,0), int(np.ceil(width*2.2)))
+        cv2.line(blur, pt1, pt2, (0,0,0), int(np.ceil(width*1.5)))
         #print(pt1,pt2)
     #gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     
-    ret, binary = cv2.threshold(blur,5,255,cv2.THRESH_BINARY)  
+    ret, binary = cv2.threshold(blur, 5, 255, cv2.THRESH_BINARY)
+    
     cv2.imshow("img2", binary)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #print("coun:",contours[0],contours[1])
 
-    c1 = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+    """c1 = sorted(contours, key=cv2.contourArea, reverse=True)[0]
     c2 = sorted(contours, key=cv2.contourArea, reverse=True)[1]
     #print("c:",c[0],c[1],c[2],c[3])
     # compute the rotated bounding box of the largest contour
@@ -46,15 +55,73 @@ def FindApple(img):
         #print(height,weight)
         crop_img = image[y2:y2 + h2, x2:x2 + w2]
     else:
-        crop_img = image[y1:y1 + h1, x1:x1 + w1]
+        crop_img = image[y1:y1 + h1, x1:x1 + w1]"""
+    c_all = sorted(contours, key = cv2.contourArea, reverse = True)
+    x1, y1, w1, h1 = cv2.boundingRect(c_all[0])
+    
+    if float((w1 * h1)) / float((height * weight)) > 0.95:  #image itself
+        print("cut")
+        c_all = c_all[1:]
+    c_new = []
+    conImgList = contourImage(c_all, False)
+    
+    for i in range(len(conImgList.flagList)):
+        x1, y1, w1, h1 = cv2.boundingRect(conImgList.flagList[i])
+        #print(w1,h1)
+        for j in range(i+1,len(conImgList.flagList)):
+            x2, y2, w2, h2 = cv2.boundingRect(conImgList.flagList[j])
+            if ((x2 > x1 and x2 < x1 + w1) and (y2 > y1 and y2  < y1 + h1) or ((x1 > x2 and x1 < x2 + w2) and (y1 > y2 and y1  < y2 + h2))) and (w2*h2 < w1*h1):  #contour c2 is in c1
+                #print("inside bigger contour")
+                conImgList.tag[j] = True
+
+    for k in range(len(conImgList.flagList)):
+        if conImgList.tag[k] == False:
+            c_new.append(conImgList.flagList[k])        
+    
+    c_new = sorted(c_new, key = cv2.contourArea, reverse = True)
+    print(len(c_new))    
+    count = 0
+    crop_imgs = []
+    while True:
+        if count == 0:
+            xi, yi, wi, hi = cv2.boundingRect(c_new[count])
+            crop_img = image[yi:yi + hi, xi:xi + wi]
+            crop_imgs.append(crop_img)
+
+
+        x1, y1, w1, h1 = cv2.boundingRect(c_new[count])
+        x2, y2, w2, h2 = cv2.boundingRect(c_new[count + 1])
+        if float(w2 * h2) / float(w1 * h1) > 0.5:
+            crop_img = image[y2:y2 + h2, x2:x2 + w2]
+            crop_imgs.append(crop_img)
+            count += 1
+        else:
+            break
+    count = 0
+    """for rawdata in crop_imgs:
+        if crop_imgs[]"""
+    result = []
+    
+    for crop_img_i in crop_imgs:
+        crop_img_i = crop_img_i[0:crop_img_i.shape[0] / 3, 0:crop_img_i.shape[1] / 2]
+        
+        result.append(crop_img_i)
+
+    for c in c_new:
+        # find bounding box coordinates
+        
+        x, y, w, h = cv2.boundingRect(c)
+        
+        cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    
     for c in contours:
         # find bounding box coordinates
         
         x, y, w, h = cv2.boundingRect(c)
         
-        cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(src, (x, y), (x + w, y + h), (0, 0, 255), 2)
     #print(crop_img.shape)
-    crop_img =crop_img[0:crop_img.shape[0]/3,0:crop_img.shape[1]/2]
+    #crop_img =crop_img[0:crop_img.shape[0]/3,0:crop_img.shape[1]/2]
     #cv2.rectangle(src, (x,y), (x+w, y+h), (0, 255, 0), 2)
     #draw_img = cv2.drawContours(src.copy(), [box], -1, (0, 0, 255), 2)
     """Xs = [i[0] for i in box]
@@ -69,9 +136,18 @@ def FindApple(img):
     #print(src.shape)
     cv2.imshow("contour", src)
     #cv2.waitKey(0)
-    return crop_img
+    #return result
+    #print(len(result))
+    """for i in range(len(result)):
+        cv2.imshow("camera",result[i])"""
+    return crop_imgs
 
-img ='0009.png'
-i = FindApple(img)
-cv2.imshow("result", i)
+img ='Apple2.png'
+images = FindApple(img)
+#print(len(images))
+count = 0
+for i in images:
+    cv2.imshow("result " + str(count), i)
+    count += 1
+    
 cv2.waitKey(0)
--- Test/model.py
+++ Test/model.py
@@ -1,104 +1,67 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
+#import torchvision
+#import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 from torch.optim import lr_scheduler
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
-
+import lsd2
+import cv2
 import numpy as np
 
-def training(epochs = 200):
-    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    print(dev)
-    batch_size = 32
-
-    model = torchvision.models.resnet18(num_classes = 6) 
-    #model = torch.load('model.pkl')
-    model = model.to(dev)
-
-    train_dl,test_dl = load_data(batch_size)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
-
-    best_accu = 0
-    for epoch in range(epochs):
-        for i, data in enumerate(train_dl):
-            (x,y) =  data
-            x = x.to(dev, dtype = torch.float)
-            y = y.to(dev, dtype = torch.long)
-            outputs = model(x)
-
-            optimizer.zero_grad()
-            loss = criterion(outputs, y)
-            loss.backward()
-            optimizer.step()
-            
-            if i%10 == 0:
-                print('Epoch [{}/{}], Loss: {:.4f}'
-                      .format(epoch + 1, epochs, loss.item()))
-            accu = testing(model,test_dl)
-            if accu > best_accu:
-                best_accu = accu
-                torch.save(model,'best_model.pkl')
-                print("accuracy is ",accu)
-    torch.save(model,'model.pkl')
-                
-                
-
-
-def load_data(bs):
-    x_train = np.load('X_train.npy')
-    x_test = np.load('X_test.npy')
-    y_train = np.load('Y_train.npy')
-    y_test = np.load('Y_test.npy')
-
-
-    x_train = np.transpose(x_train,(0,3,2,1))
-    x_test = np.transpose(x_test,(0,3,2,1))
-
-    tensor_x_train = torch.from_numpy(x_train).float() / 255
-    tensor_x_test = torch.from_numpy(x_test).float() / 255
-
-    train_ds = TensorDataset(tensor_x_train, torch.tensor(y_train))
-    test_ds = TensorDataset(tensor_x_test, torch.tensor(y_test))
-
-    train_dl = DataLoader(train_ds, batch_size=bs, shuffle = True)
-    test_dl = DataLoader(test_ds, batch_size=bs, shuffle = True)
-    return train_dl,test_dl
-
-def testing(model,test_dl):
-    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+   
     
-    batch_size = 32
-    
-    #model = torchvision.models.resnet50(num_classes = 6) 
-    
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in test_dl:
-            x,y = data
-            x = x.to(dev, dtype = torch.float)
-            y = y.to(dev, dtype = torch.long)
-            outputs = model(x)
-            _, predicted = torch.max(outputs.data, 1)
-            total += y.size(0)
-            correct += (predicted == y).sum().item()
-    accuracy = correct / total
-    return accuracy
-    
-    
+def get_images():
+    img = []
+    im = cv2.imread('000004.jpg')
+    resized = cv2.resize(im, (224,224), interpolation = cv2.INTER_AREA)
+    img.append(resized)
 
-training()
+    name = lsd2.FindApple("000006.jpg")
+    #print(name)
+    #name = ['010.png','000002.jpg','1.png','1.PNG','2.png']
+    #name = ['iphone 11','iphone8plus','11pro','11pro']
+    for each in name:
+        
+        resized = cv2.resize(each, (224,224), interpolation = cv2.INTER_AREA)
+        img.append(resized)
+    return img
 
 
-#model = torch.load('best_model.pkl')
-#dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-#model = model.to(dev)
-#train_dl,test_dl = load_data(32)
+
+
+#training(100)
+
+'''
+model = torch.load('best_model_50.pkl',map_location='cpu')
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = model.to(dev)
+train_dl,test_dl = load_data(32)
+print(testing(model,test_dl))
+'''
+
+classes = np.load('classes.npy',allow_pickle=True)
+print(classes)
+
+images = []
+model = torch.load('best_model_50.pkl',map_location='cpu')
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = model.to(dev)
+
+
+images = get_images()
+images = np.asarray(images)
+
+#resized = np.asarray(resized)
+
+x = torch.from_numpy(images).float() / 255
+x = np.transpose(x,(0,3,2,1))
+with torch.no_grad():
+    x = x.to(dev, dtype = torch.float)
+    outputs = model(x)
+    print(outputs)
+    _, predicted = torch.max(outputs.data, 1)
+    print(classes[predicted[1:]])
 #print(testing(model,test_dl))
-
