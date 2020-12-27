c0ac052265702a99d827dee3640c0a620e58a712
couponf <489399125@qq.com>
2020-12-27 11:53:33 +0800

    add cnn files

---
+++ trainCNN.py
@@ -0,0 +1,105 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch.optim import lr_scheduler
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+
+import numpy as np
+
+def training(epochs = 200):
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    print(dev)
+    batch_size = 32
+    
+    resnet50 = torchvision.models.resnet50(pretrained=True)
+    resnet50.fc = nn.Linear(2048, 2)
+ 
+    
+    model = resnet50.to(dev)
+
+    train_dl,test_dl = load_data(batch_size)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+
+    best_accu = 0
+    for epoch in range(epochs):
+        for i, data in enumerate(train_dl):
+            (x,y) =  data
+            x = x.to(dev, dtype = torch.float)
+            y = y.to(dev, dtype = torch.long)
+            outputs = model(x)
+
+            optimizer.zero_grad()
+            loss = criterion(outputs, y)
+            loss.backward()
+            optimizer.step()
+            
+            if i%10 == 0:
+                print('Epoch [{}/{}], Loss: {:.4f}'
+                      .format(epoch + 1, epochs, loss.item()))
+        accu = testing(model,test_dl)
+        if accu > best_accu:
+            best_accu = accu
+            torch.save(model,'best_model_50.pkl')
+            print("accuracy is ",accu)
+    torch.save(model,'model_50.pkl')
+                
+                
+
+
+def load_data(bs):
+    x_train = np.load('X_train.npy',allow_pickle=True)
+    x_test = np.load('X_test.npy',allow_pickle=True)
+    y_train = np.load('Y_train.npy',allow_pickle=True)
+    y_test = np.load('Y_test.npy',allow_pickle=True)
+
+    
+    x_train = np.transpose(x_train,(0,3,2,1))
+    x_test = np.transpose(x_test,(0,3,2,1))
+
+    tensor_x_train = torch.from_numpy(x_train).float() / 255
+    tensor_x_test = torch.from_numpy(x_test).float() / 255
+
+    train_ds = TensorDataset(tensor_x_train, torch.tensor(y_train))
+    test_ds = TensorDataset(tensor_x_test, torch.tensor(y_test))
+
+    train_dl = DataLoader(train_ds, batch_size=bs, shuffle = True)
+    test_dl = DataLoader(test_ds, batch_size=bs, shuffle = True)
+    return train_dl,test_dl
+
+def testing(model,test_dl):
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    
+    batch_size = 32
+    
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_dl:
+            x,y = data
+            x = x.to(dev, dtype = torch.float)
+            y = y.to(dev, dtype = torch.long)
+            outputs = model(x)
+            _, predicted = torch.max(outputs.data, 1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+    accuracy = correct / total
+    return accuracy
+    
+    
+# start training
+training(100)
+
+# checking the model
+'''
+model = torch.load('model_50.pkl')
+dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = model.to(dev)
+train_dl,test_dl = load_data(32)
+print(testing(model,test_dl))
+'''
