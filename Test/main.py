a9b53a14f9faff0c9a9d4e9afd5ace6a8956c133
FuHongju <fuhj@shanghaitech.edu.cn>
2020-12-27 12:11:11 +0800

    tidy code

--- Test/main.py
+++ Test/main.py
@@ -1,5 +1,14 @@
 import model
 import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch.optim import lr_scheduler
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import cv2
 #training(100)
 
 '''
@@ -18,7 +27,7 @@ def main():
     classes = ['iPhone8','iPhoneXR','iPhone11/12','iPhone8Plus','iPhone11Pro/12Pro','iPhoneX']
     print(classes)
 
-    images = get_images(imagePath)
+    images = model.get_images(imagePath)
     m_images = np.asarray(images)
     x = torch.from_numpy(m_images).float() / 255
     x = np.transpose(x,(0,3,2,1))
@@ -35,22 +44,22 @@ def main():
 
 
     with torch.no_grad():
-    x = x.to(dev, dtype = torch.float)
-    outputs = model1(x)
-    print(outputs[1:])
-    _, predicted1 = torch.max(outputs.data, 1)
-    print(predicted1[1:])
+        x = x.to(dev, dtype = torch.float)
+        outputs = model1(x)
+        print(outputs[1:])
+        _, predicted1 = torch.max(outputs.data, 1)
+        print(predicted1[1:])
 
-    out2 = model2(x)
-    print(out2[1:])
-    maxp, predicted2 = torch.max(out2.data, 1)
-    for i in range(len(predicted1[1:])):
-    if predicted1[1+i] == 0 and maxp[i+1] > 2.5:
+        out2 = model2(x)
+        print(out2[1:])
+        maxp, predicted2 = torch.max(out2.data, 1)
+        for i in range(len(predicted1[1:])):
+            if predicted1[1+i] == 0 and maxp[i+1] > 2.5:
 
-        print 'image ',i, 'is ',classes[predicted2[1+i]]
+                print 'image ',i, 'is ',classes[predicted2[1+i]]
 
     for i in range(1,len(images)):
-    cv2.imshow("camera"+ str(i), images[i]) 
+        cv2.imshow("camera"+ str(i), images[i]) 
     cv2.waitKey(0)
 
 
